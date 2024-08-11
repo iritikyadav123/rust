f4ea5d5b5a71f1ba334faed9f779c3cea31bee9f
iritikyadav123 <iritikyadav123@gmail.com>
2024-08-12 00:07:51 +0530

    agg

--- testing/sample1/src/index.test.ts
+++ testing/sample1/src/index.test.ts
@@ -1,5 +1,24 @@
 import { describe, expect, it } from '@jest/globals';
-import { sum } from './index';
+import { multiply, sum } from './index';
+
+describe('calculations of number', () => {
+    describe('sum of numbers', ()=> {
+        it('add 5 + 9 to equal 14', () => {
+             expect(sum(5,9)).toBe(14)
+        })
+        it('add 9 + 9 to equal 18', () => {
+            expect(sum(9,9)).toBe(18)
+       })
+    })
+    describe('multiply of number', () => {
+        it('muk 5 * 5 to be 25', () => {
+            expect(multiply(5,5)).toBe(25)
+        })
+        it('muk 9 * 5 to be 45', () => {
+            expect(multiply(9,5)).toBe(45)
+        })
+    })
+})
 
 describe('sum module', () => {
     it('adds 1 + 2 to equal 3', () => {
